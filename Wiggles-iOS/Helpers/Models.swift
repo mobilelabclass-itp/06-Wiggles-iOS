1401e746783b172d5998aa350b5c371e7e07c74d
John Henry Thompson <jht1493@gmail.com>
2022-03-04 14:26:24 -0500

    ignore

--- Wiggles-iOS/Helpers/Models.swift
+++ Wiggles-iOS/Helpers/Models.swift
@@ -7,9 +7,12 @@
 
 import SwiftUI
 
+// !!@ Not used
+
 // Lazy Navigation to load (constructor) after clicked on Button
 struct NavigationLazyView<Content: View>: View {
     let build: () -> Content
     init(_ build: @autoclosure @escaping () -> Content) { self.build = build }
     var body: Content { build() }
 }
+
